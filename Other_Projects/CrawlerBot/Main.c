a3cc9907054322a2036e033c01f34f91765c49f7
Robby <geomasta21@aol.com>
2013-02-07 20:38:55 -0600

    changed from using 16-bit to 32-bit arithmetic for pos_funct

--- Other_Projects/CrawlerBot/Main.c
+++ Other_Projects/CrawlerBot/Main.c
@@ -5,18 +5,18 @@
 #include "RASLib/init.h"
 #include "RASLib/servo.h"
 
-#define COMMAND_TIME 1000
-#define TIME_INC 10
-#define NUM_COMMANDS 3
+#define COMMAND_TIME 700
+#define TIME_INC 1
+#define NUM_COMMANDS 4
 
 LegCommand COMMANDS[2][NUM_COMMANDS] = { 
 	{ // left leg commands as {hip, knee} pairs
-		{50,0},{150,160},{150,0}
+		{180,140},{160,0},{50,0},{0,0}
 	}, 
 	{ // right leg commands as {hip, knee} pairs
-		{150,160},{150,0},{50,0}
+		{50,0},{0,0},{180,140},{160,0}
 	} 
-};
+}; 
 
 LegCommand initPos = {0, 0};
 
@@ -47,7 +47,7 @@ int main(void)
 	SetServoPosition(SERVO_0, 0);
 	
 	UARTprintf("\n\nCrawler's gotta crawl!\npress any key to continue...");
-	// getc();
+	//getc();
 	
 	firstRun = true;
 	
@@ -57,6 +57,7 @@ int main(void)
 		UARTprintf("\n executing commands!\n");
 		
 		for (i = 0; i < NUM_COMMANDS; i++) {	
+			getc();
 			UARTprintf("   command #%d...\n", i);
 			
 			if (firstRun) {
@@ -76,10 +77,9 @@ int main(void)
 	}
 }
 
-unsigned char pos_funct(unsigned char p0, unsigned char p1, unsigned short t) {
-  int res = 2*(p0 - p1)*t*t/(COMMAND_TIME*COMMAND_TIME)*t/COMMAND_TIME 
-										+ 3*(p1 - p0)*t*t/(COMMAND_TIME*COMMAND_TIME) 
-										+ p0;
+// accurate to 1 tick for 0 < b < 1676, 0 <= t <= b, abs(p0 - p1) < 255
+unsigned char pos_funct(int p0, int p1, int t, int b) {
+  int res = 2*(p0 - p1)*t*t/b*t/b/b + 3*(p1 - p0)*t*t/b/b + p0;
 	
 	if (p1 > p0 && res > p1) {
 		res = p1;
@@ -90,20 +90,8 @@ unsigned char pos_funct(unsigned char p0, unsigned char p1, unsigned short t) {
 	return (unsigned char) res;
 }
 
-void stepCommandOnce(servo_t servo, unsigned char p0, unsigned char p1, unsigned short time_ms) {
-	unsigned char pos = pos_funct(p0, p1, time_ms);
-	SetServoPosition(servo, pos);
-}
-
-void stepThruCommand(servo_t servo_a, unsigned char p0_a, unsigned char p1_a, 
-										 servo_t servo_b, unsigned char p0_b, unsigned char p1_b) {
-  unsigned short time_ms = 0;
-  
-  for (time_ms = 0; time_ms <= COMMAND_TIME; time_ms += TIME_INC) {
-		stepCommandOnce(servo_a, p0_a, p1_a, time_ms);
-		stepCommandOnce(servo_b, p0_b, p1_b, time_ms);
-		pause(TIME_INC);
-  }
+void stepCommandOnce(servo_t servo, unsigned char p0, unsigned char p1, unsigned short time_ms, unsigned short time_interval) {
+	SetServoPosition(servo, pos_funct(p0, p1, time_ms, time_interval));
 }
 
 void executeCommand(LegCommand curRight, LegCommand oldRight, LegCommand curLeft, LegCommand oldLeft) {
@@ -118,10 +106,10 @@ void executeCommand(LegCommand curRight, LegCommand oldRight, LegCommand curLeft
 								cur_left_knee = 255 - curLeft.kneePosition;
   
   for (time_ms = 0; time_ms <= COMMAND_TIME; time_ms += TIME_INC) {
-		stepCommandOnce(LEG_SERVOS[RightLeg].hipServo, old_right_hip, cur_right_hip, time_ms);
-		stepCommandOnce(LEG_SERVOS[RightLeg].kneeServo, old_right_knee, cur_right_knee, time_ms);
-		stepCommandOnce(LEG_SERVOS[LeftLeg].hipServo, old_left_hip, cur_left_hip, time_ms);
-		stepCommandOnce(LEG_SERVOS[LeftLeg].kneeServo, old_left_knee, cur_left_knee, time_ms);
+		stepCommandOnce(LEG_SERVOS[RightLeg].hipServo,  old_right_hip,  cur_right_hip,  time_ms, COMMAND_TIME);
+		stepCommandOnce(LEG_SERVOS[RightLeg].kneeServo, old_right_knee, cur_right_knee, time_ms, COMMAND_TIME);
+		stepCommandOnce(LEG_SERVOS[LeftLeg].hipServo,   old_left_hip,   cur_left_hip,   time_ms, COMMAND_TIME);
+		stepCommandOnce(LEG_SERVOS[LeftLeg].kneeServo,  old_left_knee,  cur_left_knee,  time_ms, COMMAND_TIME);
 		pause(TIME_INC);
   }
 }
